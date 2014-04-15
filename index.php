a31e8d09fdbb98ea313071821903e1a1ad1b2624
Agustin Ramos Escalante <evilnapsis@gmail.com>
2014-04-15 21:23:31 +0200

    comentando index.php #1

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-
+// el archivo autoload inicializa todos lo archivos necesarios para que el framework funcione
 include "core/autoload.php";
 
+// cargamos el modulo iniciar.
 Core::loadModule("blog");
 
 ?>
