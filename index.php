7bf6546f25af11e9c713070fd3d14c5a1b213479
Agustin Ramos Escalante <evilnapsis@gmail.com>
2014-10-12 10:13:24 -0500

    Agregando el Slim Framework

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
 // el archivo autoload inicializa todos lo archivos necesarios para que el framework funcione
-include "lib/legobox/autoload.php";
+require "lib/Slim/Slim.php";
+ include "lib/legobox/autoload.php";
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim(array("debug"=>true ));
+
+$app->get('/hello', function () {
+     //echo "<br><br><br>Hello";
+    //$app->render("index.php");
+ App::load("wellcome");
+});
+
+$app->run();
 
 // cargamos el modulo iniciar.
-App::load("wellcome");
 
 ?>
